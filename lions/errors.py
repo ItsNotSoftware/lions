11c373a061ab9b461e8531fbb17a81d6f95ce15d
Diogo Ferreira <itsnotsoftware@gmail.com>
2024-05-07 11:41:24 +0200

    improved InvalidTypeSize msg

--- lions/errors.py
+++ lions/errors.py
@@ -12,6 +12,21 @@ class DuplicateIdError(Exception):
 
 class InvalidTypeSize(Exception):
     def __init__(self, msg_name, field_name, type, size):
+        expected_sizes = {
+            "bool": 1,
+            "int8_t": 1,
+            "uint8_t": 1,
+            "int16_t": 2,
+            "uint16_t": 2,
+            "int32_t": 4,
+            "uint32_t": 4,
+            "int64_t": 8,
+            "uint64_t": 8,
+            "float": 4,
+            "double": 8,
+            "string": ">1",
+        }
+
         super().__init__(
-            f'Invalid size "{size}" for type "{type}" in field "{field_name}" in message "{msg_name}"'
+            f'Invalid size "{size}" for type "{type}" in field "{field_name}" in message "{msg_name}". Size must be {expected_sizes[type]}'
         )
