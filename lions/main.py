7697cf36fe2199b002c461164c491296a4e83091
Diogo Ferreira <itsnotsoftware@gmail.com>
2024-05-08 15:59:53 +0200

    pretty prints

--- lions/main.py
+++ lions/main.py
@@ -2,6 +2,32 @@ import sys
 from lions.cpp_gen.cpp_generator import CppGenerator
 from lions.yaml_parser import YamlParser
 import colorama
+from colorama import Fore, Style
+
+
+def print_title():
+    # Display a title
+    print(Fore.GREEN)
+
+    print(
+        """
+██╗     ██╗ ██████╗ ███╗   ██╗███████╗
+██║     ██║██╔═══██╗████╗  ██║██╔════╝
+██║     ██║██║   ██║██╔██╗ ██║███████╗
+██║     ██║██║   ██║██║╚██╗██║╚════██║
+███████╗██║╚██████╔╝██║ ╚████║███████║
+╚══════╝╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚══════╝
+"""
+    )
+    print(Style.RESET_ALL + "\nInitiating compilation of messages...\n")
+
+
+def print_success_message():
+    # Display success message
+    success_message = "All messages compiled successfully!"
+    print(Fore.GREEN + "+" + "-" * (len(success_message) + 2) + "+" + Style.RESET_ALL)
+    print(Fore.GREEN + f"| {success_message} |" + Style.RESET_ALL)
+    print(Fore.GREEN + "+" + "-" * (len(success_message) + 2) + "+" + Style.RESET_ALL)
 
 
 def main():
@@ -15,8 +41,12 @@ def main():
     msg_files_dir = sys.argv[1]
     output_dir = sys.argv[2]
 
+    print_title()
+
     parser = YamlParser(msg_files_dir)
     cpp_generator = CppGenerator(output_dir)
 
     for filename, msgs in parser.parse_file():
         cpp_generator.generate_msg_files(filename, msgs)
+
+    print_success_message()
