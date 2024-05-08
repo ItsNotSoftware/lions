1a0787a7949adb7e440de544bdc771d8f75d0767
Diogo Ferreira <itsnotsoftware@gmail.com>
2024-05-08 16:14:54 +0200

    more eamples

--- lions/main.py
+++ lions/main.py
@@ -35,7 +35,7 @@ def main():
 
     # Check if the number of arguments is correct
     if len(sys.argv) != 3:
-        print("Usage: lions.main <msg_files_dir> <output_dir>")
+        print("Usage: lions <msg_files_dir> <output_dir>")
         sys.exit(1)
 
     msg_files_dir = sys.argv[1]
