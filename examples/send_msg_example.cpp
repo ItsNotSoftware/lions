4c313c459b8f90d7d0546882260ba793601ec8e8
Diogo Ferreira <itsnotsoftware@gmail.com>
2024-05-10 10:59:19 +0200

    fixed no payload message generation

--- examples/send_msg_example.cpp
+++ examples/send_msg_example.cpp
@@ -2,25 +2,29 @@
 
 #include <iostream>
 
-#include "generated_code/example.hpp"
 #include "generated_code/lions.hpp"
+#include "generated_code/my_messages.hpp"
 
 // Define a constant for the device identifier used in the message.
 constexpr uint8_t this_device_id = 1;
 constexpr uint8_t brodcast_id = 255;
 
 // System-specific implementation of sending a byte.
-void send();
+void send(uint8_t byte) {
+    // Implementation of sending a byte.
+}
 
 // System-specific implementation of sending an array of bytes.
-void send_array(uint8_t *data, uint8_t size);
+void send_array(uint8_t *data, uint8_t size) {
+    // Implementation of sending an array of bytes.
+}
 
-void send_msg(LMsg &encoded_msg) {
+void send_msg(lions::LMsg &encoded_msg) {
     // Send each component of the message header.
-    send(encoded_msg.src);       // Source device ID
-    send(encoded_msg.dst);       // Destination device ID
-    send(encoded_msg.msg_id);    // Message ID
-    send(encoded_msg.next_hop);  // Next hop ID
+    send(encoded_msg.header.src);       // Source device ID
+    send(encoded_msg.header.dst);       // Destination device ID
+    send(encoded_msg.header.msg_id);    // Message ID
+    send(encoded_msg.header.next_hop);  // Next hop ID
 
     // Send the payload.
     send_array(encoded_msg.payload, encoded_msg.payload_size);
@@ -29,8 +33,8 @@ void send_msg(LMsg &encoded_msg) {
 void send_msg_example(uint8_t dst_id, uint8_t next_hop) {
     int16_t sound_level = -23;
 
-    // Create a microphone message with the string "Hello World!"
-    lions::MicrophoneMsg msg(this_device_id, dst_id, next_hop, sound_level, "Hello World!");
+    // Create a microphone message with: soundlevel = -23,  string = "Hello World!"
+    lions::MicrophoneMsg msg(sound_level, "Hello World!");
 
     // Encode the message for sending.
     auto encoded_msg = msg.encode(this_device_id, 2, 3);
