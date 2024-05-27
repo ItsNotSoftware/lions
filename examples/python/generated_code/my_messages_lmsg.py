f901f399b5c865865ee1f0b53fb6f5d97ff2ffd7
Diogo Ferreira <itsnotsoftware@gmail.com>
2024-05-27 10:28:46 +0200

    python generation

--- examples/python/generated_code/my_messages_lmsg.py
+++ examples/python/generated_code/my_messages_lmsg.py
@@ -42,31 +42,31 @@ class AccelerometerMsg:
     @staticmethod
     def from_lmsg(msg: LMsg) -> "AccelerometerMsg":
         
-        acc_x = struct.unpack("<float", msg.payload[0:4])[0]
-        acc_y = struct.unpack("<float", msg.payload[4:8])[0]
-        acc_z = struct.unpack("<float", msg.payload[8:12])[0] 
+        acc_x = struct.unpack("<f", msg.payload[0:4])[0]
+        acc_y = struct.unpack("<f", msg.payload[4:8])[0]
+        acc_z = struct.unpack("<f", msg.payload[8:12])[0] 
         instance = AccelerometerMsg(acc_x, acc_y, acc_z)
         instance.header = msg.header
 
         return instance
 
     def encode(self, src: int, dst: int, next_hop: int) -> LMsg:
-        msg = LMsg()
+        msg = LMsg(12)
         msg.header.src = src
         msg.header.dst = dst
         msg.header.next_hop = next_hop
         msg.header.msg_id = 1 
         
-        msg.payload[0:4] = struct.pack("<float", self.acc_x) 
-        msg.payload[4:8] = struct.pack("<float", self.acc_y) 
-        msg.payload[8:12] = struct.pack("<float", self.acc_z) 
+        msg.payload[0:4] = struct.pack("<f", self.acc_x) 
+        msg.payload[4:8] = struct.pack("<f", self.acc_y) 
+        msg.payload[8:12] = struct.pack("<f", self.acc_z) 
         msg.calculate_checksum()
 
         return msg
 
 
     def __str__(self) -> str:
-        s = "[AccelerometerMsg]\n"
+        s = "\n[AccelerometerMsg]\n"
         s += f"    {self.header}\n\n"
         s += f"    acc_x: {self.acc_x}\n" 
         s += f"    acc_y: {self.acc_y}\n" 
@@ -83,7 +83,7 @@ class MicrophoneMsg:
     @staticmethod
     def from_lmsg(msg: LMsg) -> "MicrophoneMsg":
         
-        sound_level = struct.unpack("<int", msg.payload[0:2])[0]
+        sound_level = struct.unpack("<h", msg.payload[0:2])[0]
         message = msg.payload[2:102].decode('utf-8')
          
         instance = MicrophoneMsg(sound_level, message)
@@ -92,13 +92,13 @@ class MicrophoneMsg:
         return instance
 
     def encode(self, src: int, dst: int, next_hop: int) -> LMsg:
-        msg = LMsg()
+        msg = LMsg(102)
         msg.header.src = src
         msg.header.dst = dst
         msg.header.next_hop = next_hop
         msg.header.msg_id = 2 
         
-        msg.payload[0:2] = struct.pack("<int", self.sound_level) 
+        msg.payload[0:2] = struct.pack("<h", self.sound_level) 
         msg.payload[2:102] = self.message.encode('utf-8')
          
         msg.calculate_checksum()
@@ -107,7 +107,7 @@ class MicrophoneMsg:
 
 
     def __str__(self) -> str:
-        s = "[MicrophoneMsg]\n"
+        s = "\n[MicrophoneMsg]\n"
         s += f"    {self.header}\n\n"
         s += f"    sound_level: {self.sound_level}\n" 
         s += f"    message: {self.message}\n" 
@@ -127,7 +127,7 @@ class PingMsg:
         return instance
 
     def encode(self, src: int, dst: int, next_hop: int) -> LMsg:
-        msg = LMsg()
+        msg = LMsg(0)
         msg.header.src = src
         msg.header.dst = dst
         msg.header.next_hop = next_hop
@@ -139,7 +139,7 @@ class PingMsg:
 
 
     def __str__(self) -> str:
-        s = "[PingMsg]\n"
+        s = "\n[PingMsg]\n"
         s += f"    {self.header}\n\n"
         
         return s
--- examples/python/test.py
+++ examples/python/test.py
@@ -1,5 +1,24 @@
 from generated_code.my_messages_lmsg import *
 
 
-acc = AccelerometerMsg(1, 2, 34.12232)
+acc = AccelerometerMsg(1, 2, 34)
 print(acc)
+msg = acc.encode(1, 2, 3)
+acc = AccelerometerMsg.from_lmsg(msg)
+print(acc)
+
+print()
+
+mic = MicrophoneMsg(-123, "Hello World!")
+print(mic)
+msg = mic.encode(4, 5, 6)
+mic = MicrophoneMsg.from_lmsg(msg)
+print(mic)
+
+print()
+
+ping = PingMsg()
+print(ping)
+msg = ping.encode(7, 8, 9)
+ping = PingMsg.from_lmsg(msg)
+print(ping)
