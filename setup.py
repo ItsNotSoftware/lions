72692f7721969dd00032e994185cd20ebe84fd0f
Diogo Ferreira <itsnotsoftware@gmail.com>
2024-06-10 10:23:03 +0200

    Fixed templates not being uploaded

--- setup.py
+++ setup.py
@@ -5,13 +5,20 @@ with open("README.md", "r") as f:
 
 setup(
     name="lionsc",
-    version="2.0.1",
+    version="2.0.4",
     author="Diogo Ferreira",
     author_email="itsnotsoftware@gmail.com",
     description="LIONS is a communication protocol coupled with a compiler (lionsc), specifically designed for low-bandwidth IoT mesh and ad hoc networks.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    package_data={"lions": ["cpp_gen/templates/*.jinja"]},
+    package_data={
+        "lionsc": [
+            "code_generation/cpp/templates/*.jinja",
+            "code_generation/python/templates/*.jinja",
+            "code_generation/js/templates/*.jinja",
+            "code_generation/ts/templates/*.jinja",
+        ]
+    },
     url="https://github.com/ItsNotSoftware/lions",
     license="MIT",
     classifiers=[
